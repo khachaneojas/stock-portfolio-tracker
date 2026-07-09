3bea04fdda21756a5b0a7a78e7f309c8f87d02fc
khachaneojas <145201556+khachaneojas@users.noreply.github.com>
2026-07-09 15:12:30 +0100

    Initial commit

---
+++ src/main/java/com/stocktracker/stock_portfolio_tracker/StockPortfolioTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.stocktracker.stock_portfolio_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPortfolioTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPortfolioTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stocktracker/stock_portfolio_tracker/StockPortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocktracker.stock_portfolio_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPortfolioTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
